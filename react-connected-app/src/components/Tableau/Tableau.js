c607669142f0338750a65a496941bf9d643f433e
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-01-30 21:43:26 -0600

    adding the Tableau <script> tag

--- react-connected-app/src/components/Tableau/Tableau.js
+++ react-connected-app/src/components/Tableau/Tableau.js
@@ -1,7 +1,18 @@
-function Tableau() {
-  return {
+import { useState, useEffect } from "react"
+import { Helmet } from "react-helmet" // adds <script> tag to the document head
 
-  }
+
+function Tableau() {
+  return (
+    <>
+      <Helmet> 
+        <script type="module" src="https://embedding.tableauusercontent.com/tableau.embedding.3.0.0.min.js" async></script>
+      </Helmet>
+      <div>
+
+      </div>
+    </>
+  );
 }
 
 export default Tableau;
