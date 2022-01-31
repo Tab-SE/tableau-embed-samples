622be0134290597328bfa0dca0508aa9d4c4e71f
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-01-30 22:08:50 -0600

    importing Viz component

--- react-connected-app/src/components/Tableau/Tableau.js
+++ react-connected-app/src/components/Tableau/Tableau.js
@@ -1,16 +1,14 @@
-import { useState, useEffect } from "react"
 import { Helmet } from "react-helmet" // adds <script> tag to the document head
+import Viz from "./Viz/Viz.js"
 
 
-function Tableau() {
+function Tableau(props) {
   return (
     <>
       <Helmet> 
         <script type="module" src="https://embedding.tableauusercontent.com/tableau.embedding.3.0.0.min.js" async></script>
       </Helmet>
-      <div>
-
-      </div>
+      <Viz/>
     </>
   );
 }
